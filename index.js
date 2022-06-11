7a15887de4120240b614254e158ab08392019d1a
BrunodevBandeira <brunodev29ab@gmail.com>
2022-06-11 17:56:51 -0300

    15%

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
+const port = 3000;
+const path = require("path");
+
+//ENGINE
+app.set("view engine", "ejs");
 
 //ROTA
 app.get("/", (req, res) => {
-  res.send("Ola Mundo.");
+  res.render("index");
 });
 
-app.listen(3000);
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
